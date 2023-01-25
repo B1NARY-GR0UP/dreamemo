6a1dcf2223b110d5da043de93f65a8c5bfcee3c0
Lorain <743804605@qq.com>
2023-01-25 22:27:00 +0800

    add thrift protobuf idl

---
+++ protocol/thrift/dreamemo.thrift
@@ -0,0 +1,15 @@
+namespace go thrift
+
+struct GetRequest {
+    1: string group
+    2: string key
+}
+
+struct GetResponse {
+    1: optional byte value
+    2: optional double qps
+}
+
+service Memo {
+    GetResponse Get(1: GetRequest req)
+}
